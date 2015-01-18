115f6d63ca3a72dff1ad95594724597c77f1c588
Jim Veneskey <github@strifecrafter.com>
2015-01-18 14:12:35 -0500

    Tutorial #8, Drawing Shapes, Text and Window Titles - and upgrading the game to "Ham Blaster 2.0" - even more Ham than 1.0!

--- src/javagame/Game.java
+++ src/javagame/Game.java
@@ -5,7 +5,7 @@ import org.newdawn.slick.state.*;
 
 public class Game extends StateBasedGame {
 	
-	public static final String gamename = "Ham Blaster!";
+	public static final String gamename = "Ham Blaster! 2.0";
 	public static final int menu = 0;
 	public static final int play = 1;
 
@@ -26,7 +26,7 @@ public class Game extends StateBasedGame {
 		AppGameContainer appgc;
 		try {
 			appgc = new AppGameContainer(new Game(gamename));
-			appgc.setDisplayMode(640, 360, false);
+			appgc.setDisplayMode(1280, 1024, false);
 			appgc.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -12,12 +12,15 @@ public class Menu extends BasicGameState {
 		
 	}
 	
+	// 50,50 = x,y
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		g.drawString("Are you ready to blast some ham!?!?",50,50);
+		g.drawRect(50, 100, 60, 120);  // x,y,width,height
+		g.drawOval(200,130, 130, 80); // x,y,width,height
 		
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-		
 	}
 
 	public int getID() {
