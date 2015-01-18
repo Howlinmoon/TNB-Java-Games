8237ff0629f4b791ebe10130aa8102cbad9b51b7
Jim Veneskey <github@strifecrafter.com>
2015-01-18 11:15:08 -0500

    Tutorial #5 - building a main class for the game

--- src/javagame/Game.java
+++ src/javagame/Game.java
@@ -1,6 +1,20 @@
 package javagame;
 
-public class Game {
+import  org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Game extends StateBasedGame {
+	
+	public static final String gamename = "Ham Blaster!";
+	public static final int menu = 0;
+	public static final int play = 1;
+
+	
+	public Game(String gamename) {
+		super(gamename);
+		this.addState(new Menu(menu));
+		this.addState(new Play(play))
+	}
 
 	public static void main(String[] args) {
 
