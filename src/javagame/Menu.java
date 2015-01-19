697fedf78df450c7ec3056a6ace3b8d306ad8916
Jim Veneskey <github@strifecrafter.com>
2015-01-18 20:19:05 -0500

    Tutorial #10 - Mouse Movement Input Now displays the X and Y co-ordinates of the mouse, and it also is displaying the co-ordinates AT the mouse position as an experiment in positioning text on the fly.

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -1,9 +1,14 @@
 package javagame;
 
+import org.lwjgl.input.Mouse;
 import  org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState {
+	
+	public String mouse = "No Input Yet!";
+	int xpos = 100;
+	int ypos = 100;
 	public Menu(int state) {
 		
 	}
@@ -14,7 +19,8 @@ public class Menu extends BasicGameState {
 	
 	// 50,50 = x,y
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawString("Are you ready to blast some ham!?!?",50,50);
+		g.drawString(mouse,xpos,1024 - ypos);
+		g.drawString(mouse,50,50);
 
 		// experimenting with creating a border around the picture
 		for (int offset = 0; offset < 10; offset++) {
@@ -30,6 +36,11 @@ public class Menu extends BasicGameState {
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+//		int xpos = Mouse.getX();
+//		int ypos = Mouse.getY();
+		xpos = Mouse.getX();
+		ypos = Mouse.getY();
+		mouse = "Mouse X position: "+xpos+", Y position: "+ypos;
 	}
 
 	public int getID() {
